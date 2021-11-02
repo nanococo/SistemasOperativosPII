2683664992749ed1ff751b7a06d7bc3ccab3e926
Eduardo <edumadrim@gmail.com>
2021-11-02 12:32:56 -0600

    revised spy & constant .h

--- spy.c
+++ spy.c
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #include <sys/shm.h>
 #include <semaphore.h>
+#include <pthread.h>
 
-void printMemmory(short *mem, int memSize, sem_t *mutex);
-void printProcessesStatus(int exec_shm_id, int blocked_shm_id, int current_shm_id);
+#include "constants.h"
+
+void printMemmory(pthread_t *mem, int memSize, sem_t *mutex);
+void printProcessesStatus(pthread_t *exec_shm, pthread_t *blocked_shm, pthread_t *current_shm, sem_t *mutex);
 
 int main(int argc, char **argv)
 {
     if (argc == 5)
     {
-        int mem_shm_id      = atoi(argv[1]);
-        // int exec_shm_id     = argv[2];
-        // int blocked_shm_id  = argv[3];
-        // int current_shm_id  = argv[4];
+        int mem_shm_id = atoi(argv[1]);
+        int exec_shm_id = atoi(argv[2]);
+        int blocked_shm_id = atoi(argv[3]);
+        int current_shm_id = atoi(argv[4]);
 
+        pthread_t *mem_shm = shmat(mem_shm_id, NULL, 0);
+        pthread_t *exec_shm = shmat(exec_shm_id, NULL, 0);
+        pthread_t *blocked_shm = shmat(blocked_shm_id, NULL, 0);
+        pthread_t *current_shm = shmat(current_shm_id, NULL, 0);
 
-        short *mem          = shmat(mem_shm_id, NULL, 0);
+        sem_t *mem_mutex = sem_open(MEMORY_MUTEX_NAME, 0);
+        sem_t *spy_mutex = sem_open(SPY_MUTEX_NAME, 0);
 
-        sem_t *mem_mutex    = sem_open("Memory Mutex", 0);
-
-        printMemmory(mem, 5, mem_mutex);
-
+        // ? Deberia evitar que alguno entre a la memoria para imprimir el actual?
+        printMemmory(mem_shm, 5, mem_mutex);
+        printProcessesStatus(exec_shm,blocked_shm,current_shm,spy_mutex);
     }
     else
     {
@@ -30,11 +37,11 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void printMemmory(short *mem, int memSize, sem_t *mutex){
-    
-    const char lineString[]  = "             ----- \n";
+void printMemmory(pthread_t *mem, int memSize, sem_t *mutex)
+{
+    const char lineString[] = "             ----- \n";
+
     printf("Memory: \n");
-    
 
     sem_wait(mutex);
     for (int line = 0; line < memSize; line++)
@@ -43,11 +50,34 @@ void printMemmory(short *mem, int memSize, sem_t *mutex){
         printf(lineString);
     }
     sem_post(mutex);
-    
-    //printf(lineString);
-
 }
 
-void printProcessesStatus(int exec_shm_id, int blocked_shm_id, int current_shm_id){
+void printProcessesStatus(pthread_t *exec_shm, pthread_t *blocked_shm, pthread_t *current_shm, sem_t *mutex)
+{
+    printf("Current Process in Memory: \n");
 
+    printf("%d \n", current_shm[0]);
+
+    printf("Blocked Processes: \n");
+
+    for (int i = 0; i < BLOCKED_SHM_SIZE; i++)
+    {
+        pthread_t pid = blocked_shm[i];
+        if (pid != 0)
+        {
+            printf("%d \n", pid);
+        }
+    }
+
+    printf("Executing Processes: \n");
+
+    for (int i = 0; i < EXEC_SHM_SIZE; i++)
+    {
+        pthread_t pid = exec_shm[i];
+        if (pid != 0)
+        {
+            printf("%d \n", pid);
+        }
+    }
+    
 }
